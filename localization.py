4d261bd5c805835321904835558dc0589bd7a559
Spamium <hugo00pereira@gmail.com>
2020-11-10 18:17:08 +0000

    Add files via upload

--- localization.py
+++ localization.py
@@ -1,16 +1,51 @@
+import datetime
+import math
+
 class localization:
 	
+	# this is our last position
+	position = [0, 0, 0]
+	beaconPosition = []
+	
+	# uncertainties
+	positionUncert
 	
 	
-	
-	def __init__(Self):
-	
-	
+	lastTime = 0
+	deltaT = 0
 	
 	
+	def __init__(self, beaconList):
+		lastTime = datetime.now()
+		# TODO - INITIALIZE BEACONS
+		
 	
+	def kalmanStep(self):
+		
+		# deltaT from last step
+		deltaT = datetime.now() - lastTime
+		
+		# location estimate with basic equation of motion
+		inertialEst = self.position + getfromDVL()*deltaT + 0.5*getFromIMU()*deltaT*deltaT
+	
+		# relative estimation
+		relativeEst = getFromLBL()
+		
+		
 	
 	def GetFromIMU(self):
 	
 	def GetFromDVL(self):
 	
+	def getFromLBL(self):
+		
+		distance = []
+		for b in beaconPosition:
+			distance.append( (b[1], calcDist(b[0]) )
+			
+		
+		
+	def calcDist(self, beacon):
+	
+	
+	def 

85b1518c9eda0474e86ce7951e9cd92daf62a03d
Spamium <hugo00pereira@gmail.com>
2020-12-07 16:13:51 +0000

    Update Kalman_UUV.py

--- Kalman_UUV.py
+++ Kalman_UUV.py
@@ -8,6 +8,9 @@ Created on Mon Dec  7 15:57:34 2020
 class Kalman_UUV():
     
     
+    DVL = open('DLV.txt', r)
+    IMU = open('IMU.txt', r)
+    
     pos = [0, 0, 0]
     vel = [0, 0, 0]
     accel = [0, 0, 0]
@@ -23,8 +26,16 @@ class Kalman_UUV():
     
     def predict(self):
         
+        velk = self.pos + getSpeed()*self.deltaT + (1/2)*getAccel()*(self.deltaT**2)
         
         
+    
+    # get reading from DVL
+    # provisionally, read from file DVL.txt
+    def getSpeed(self):
         
         
+    # get reading from IMU
+    # provisionally, read from file IMU.txt
+    def getAccel(self):
         

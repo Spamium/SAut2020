bbb65bbd7d4657305f5f949306dc247f2f0aecaf
Spamium <hugo00pereira@gmail.com>
2020-12-07 16:01:34 +0000

    Create Kalman_UUV.py

---
+++ Kalman_UUV.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 15:57:34 2020
+
+@author: hugo0
+"""
+
+class Kalman_UUV():
+    
+    
+    pos = [0, 0, 0]
+    vel = [0, 0, 0]
+    accel = [0, 0, 0]
+    
+    
+    def __init__(self):
+        
+        
+        
+        return
+    
+    
+    
+    def predict(self):
+        
+        
+        
+        
+        
+        
